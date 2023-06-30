2230d0422a0baff3292a423714ed46889b769013
Etti <se.ettenauer@gmail.com>
2023-06-30 22:17:06 +0200

    Test

--- Model/Configurations/ShoppingDbContext.cs
+++ Model/Configurations/ShoppingDbContext.cs
@@ -6,6 +6,7 @@ public class ShoppingDbContext : DbContext{
     public ShoppingDbContext(DbContextOptions<ShoppingDbContext> options) : base(options) {
         
     }
+    
     protected override void OnModelCreating(ModelBuilder builder) {
         base.OnModelCreating(builder);
     }
